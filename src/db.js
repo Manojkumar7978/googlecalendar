ff802292dd503d59f32c445645b89e6124aa2ad9
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2024-02-14 16:51:18 +0530

    Google Calander

---
+++ src/db.js
@@ -0,0 +1,25 @@
+let events = [
+    {
+      id: 1,
+      title: 'Meeting',
+      start: new Date(2024, 1, 14, 10, 0, 0),
+      end: new Date(2024, 1, 14, 12, 0, 0),
+      type: 'normal'
+    },
+    {
+      id: 2,
+      title: 'Gym',
+      start: new Date(2024, 1, 14, 14, 0, 0),
+      end: new Date(2024, 1, 14, 16, 0, 0),
+      type: 'stretching'
+    },
+    {
+      id: 3,
+      title: 'Family Dinner',
+      start: new Date(2024, 1, 14, 18, 0, 0),
+      end: new Date(2024, 1, 14, 20, 0, 0),
+      type: 'normal'
+    }
+  ];
+
+  export default events;
---
+++ src/month.js
@@ -0,0 +1,15 @@
+import dayjs from "dayjs";
+
+export function getMonth(month=dayjs().month()){
+    const year=dayjs().year()
+    const firstDayofTheMonth=dayjs(new Date(year,month,-1)).day()
+    let currentMonthCount=0-firstDayofTheMonth
+
+    const dayMatrix=new Array(5).fill([]).map(()=>{
+        return new Array(7).fill(null).map(()=>{
+            currentMonthCount++;
+            return dayjs(new Date(year,month,currentMonthCount))
+        })
+    })
+    return dayMatrix
+}
